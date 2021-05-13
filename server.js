f0621d4de1b0451a0f5bbb7daee16bd3095a9461
khoaHyh <khoahuynh.toronto@gmail.com>
2021-05-13 15:25:20 -0400

    Remove https config code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const http = require("http");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
@@ -12,10 +11,6 @@ const passport = require("passport");
 const auth = require("./auth");
 const authRoutes = require("./routes/auth");
 const { pool } = require("./db/db");
-
-const privateKey = fs.readFileSync(process.env.KEY, "utf8");
-const certificate = fs.readFileSync(process.env.CERT, "utf8");
-const credentials = { key: privateKey, cert: certificate };
 
 // Allow app to use passport strategies
 auth(passport);
